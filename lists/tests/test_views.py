32b580ce31ba1106b1626f8a21a637d4193144d5
thefonso <thefonso@thefonso.com>
2018-09-05 03:55:07 -0500

    15.4 begins

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.utils.html import escape
 from lists.models import Item, List
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
-
+from unittest import skip
 
 class HomePageTest(TestCase):
 
@@ -58,13 +58,11 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-
     def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
-
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
@@ -81,7 +79,6 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
 
-
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -95,7 +92,6 @@ class ListViewTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
-
 
     def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
@@ -136,3 +132,17 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
