2e6a8077da7f30063eb692a53a9818d3a858b234
thefonso <thefonso@thefonso.com>
2018-09-27 04:19:18 -0500

    Finishing Off Our FT, Testing Logout

--- superlists/settings.py
+++ superlists/settings.py
@@ -141,11 +141,14 @@ LOGGING = {
     'root': {'level': 'INFO'},
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'alephmage@gmail.com'
+EMAIL_HOST_USER = 'badbabykosh@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_TO_EMAIL = EMAIL_HOST_USER
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
