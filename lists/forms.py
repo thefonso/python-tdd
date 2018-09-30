1039c36bda0200f7699e57cef7c6e355e8c0e12b
thefonso <thefonso@thefonso.com>
2018-09-29 22:43:57 -0500

    23.7 begins

--- lists/forms.py
+++ lists/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from lists.models import Item
+from lists.models import Item, List
 from django.core.exceptions import ValidationError
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
@@ -26,8 +26,13 @@ class ItemForm(forms.models.ModelForm):
         return super().save()
 
 
-class NewListForm(object):
-    pass
+class NewListForm(ItemForm):
+
+    def save(self, owner):
+        if owner.is_authenticated:
+            List.create_new(first_item_text=self.cleaned_data['text'], owner=owner)
+        else:
+            List.create_new(first_item_text=self.cleaned_data['text'])
 
 
 class ExistingListItemForm(ItemForm):
